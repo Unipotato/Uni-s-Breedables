44daccd475c5e625e65d0c6295592c196e0e7ac1
Unipotato <dzakmal@gmail.com>
2017-01-27 22:29:57 +0800

    update

--- static/config.js
+++ static/config.js
@@ -19,4 +19,4 @@ var contactUrl = "http://reddit.com/u/UniLune";
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "";
